cab774c805572fd02cf3445ead65984944df2170
Danyelle <dkobes15@gmail.com>
2025-03-01 11:10:25 -0600

    fixing classes and making id into a number instead of string

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -5,21 +5,32 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-const filePath = path.join(__dirname, 'searchHistory.json');
-
 // TODO: Define a City class with name and id properties
-type City = {
+class City {
   name: string;
-  id: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
 }
 
 // TODO: Complete the HistoryService class
 class HistoryService {
+  filePath: string;
+  cities: City[];
+
+  constructor() {
+    this.filePath = path.join(__dirname, '../../db/searchHistory.json');
+    this.cities = [];
+  }
+
+
   // TODO: Define a read method that reads from the searchHistory.json file
    private async read(): Promise<City[]> {
     return new Promise((resolve, reject) => {
-      fs.readFile(filePath, 'utf8', (err, data) => {
+      fs.readFile(this.filePath, 'utf8', (err, data) => {
         if (err) {
           if (err.code === 'ENOENT') {
             resolve([]);
@@ -36,7 +47,7 @@ class HistoryService {
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   private async write(cities: City[]): Promise<void> {
     return new Promise((resolve, reject) => {
-      fs.writeFile(filePath, JSON.stringify(cities, null, 2), 'utf8', (err) => {
+      fs.writeFile(this.filePath, JSON.stringify(cities, null, 2), 'utf8', (err) => {
         if (err) {
           reject(err);
         } else {
@@ -55,8 +66,8 @@ class HistoryService {
    async addCity(city: string): Promise<void> {
     const cities = await this.read();
     const newCity: City = {
-      id: (cities.length + 1).toString(),
-      name: city,
+      id: cities.length + 1,
+      name: city
     };
     cities.push(newCity);
     await this.write(cities);
